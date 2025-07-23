6f29e93b290e2c8c05b494cf3a72485528bd84d7
nikhil-tej-arepally <2400030129@kluniversity.in>
2025-07-23 09:56:16 +0530

    v1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port: 2345}
+  server: {port: 2345},
+  base: '/neww/'
 })
